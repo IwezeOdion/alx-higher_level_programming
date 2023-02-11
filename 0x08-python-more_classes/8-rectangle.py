737b7a2558f6b358ae020b2abd135f304a7ed58e
Iwezeodion <iwezeodion@gmail.com>
2023-02-10 17:31:41 -0800

    Cool

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -8,6 +8,7 @@ class Rectangle:
     """ Rectangle """
 
     number_of_instances = 0
+    print_symbol = '#'
 
     def __init__(self, width=0, height=0):
         """ initialize """
@@ -22,7 +23,7 @@ class Rectangle:
         ret = ''
         for i in range(self.__height):
             for j in range(self.__width):
-                ret += '#'
+                ret += str(self.print_symbol)
             ret += '\n'
         return ret[:-1]
 
@@ -72,3 +73,15 @@ class Rectangle:
         if self.__height == 0 or self.__width == 0:
             return 0
         return 2 * (self.__width + self.__height)
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        """ bigger_or_equal """
+        if type(rect_1) is not Rectangle:
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if type(rect_2) is not Rectangle:
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if rect_1.area() >= rect_2.area():
+            return rect_1
+        if rect_1.area() < rect_2.area():
+            return rect_2
