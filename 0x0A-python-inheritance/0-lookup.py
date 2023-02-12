79268518989c24284693ca39961e75f3fb1e6597
Iwezeodion <iwezeodion@gmail.com>
2023-02-11 16:55:09 -0800

    TaSK

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,13 @@
+
+n that returns the list of available
+attributes and methods of an object"""
+
+
+def lookup(obj):
+        """Returns attributes amd methods of an object
+            Args:
+                        obj: The object
+                                Return: The attributes and the methods of an object
+                                    """
+
+                                        return dir(obj)
